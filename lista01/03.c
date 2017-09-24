7a125e1da418f836abc685db1593c37f874e6e37
Carlos Eduardo Da Silva <ceduardo@MacBook-Pro-de-Carlos.local>
2017-09-24 11:06:33 -0300

    adicionado exercicio 03

---
+++ lista01/03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x, y, resultado;
+    printf("Digite dois valores: ");
+    scanf("%d %d", &x, &y);
+
+    resultado = x - y;
+
+    printf("%d\n", resultado);
+
+    return 0;
+
+}
